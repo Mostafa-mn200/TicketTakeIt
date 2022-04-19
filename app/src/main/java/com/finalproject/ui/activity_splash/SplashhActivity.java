a6df799a30d41047f25ec9577ac378727da55b43
mahmo <mahmoudl_@outlook.com>
2022-04-19 15:26:56 +0200

    kenzo

--- app/src/main/java/com/finalproject/ui/activity_splash/SplashhActivity.java
+++ app/src/main/java/com/finalproject/ui/activity_splash/SplashhActivity.java
@@ -1,9 +1,9 @@
 package com.finalproject.ui.activity_splash;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +11,13 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import com.finalproject.R;
-import com.finalproject.databinding.ActivitySplashBinding;
-import com.finalproject.language.Language;
+
+import com.finalproject.databinding.ActivitySplashhBinding;
 import com.finalproject.preferences.Preferences;
 import com.finalproject.ui.activity_base.BaseActivity;
 import com.finalproject.ui.activity_login.LoginActivity;
 import com.finalproject.ui.user.activity_home.HomeActivity;
 
-import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import io.paperdb.Paper;
@@ -29,23 +28,24 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
-public class SplashActivity extends BaseActivity {
-    private ActivitySplashBinding binding;
+public class SplashhActivity extends BaseActivity {
+    private ActivitySplashhBinding binding;
     private CompositeDisposable disposable = new CompositeDisposable();
     private Preferences preferences;
     private String lang;
 
     private Animation animation1;
     private Animation animation2;
+    private Animation animation3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_splash);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_splashh);
         initView();
-    }
 
+    }
     private void initView() {
         Paper.init(this);
         lang = getLang();
@@ -76,27 +76,30 @@ public class SplashActivity extends BaseActivity {
 
                     }
                 });
-    }
+        navigateToLoginActivity();
+        navigateToHomeActivity();
 
-    private void animateMethod() {
+    }private void animateMethod() {
 
         animation1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.slide_up);
         animation2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.slide_up);
-        binding.logo.setVisibility(View.VISIBLE);
+        animation3 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.slide_up);
+        binding.tLogo.setVisibility(View.VISIBLE);
 
 
-        binding.logo.startAnimation(animation1);
+        binding.tLogo.startAnimation(animation1);
 
 
         animation1.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                binding.logo.setVisibility(View.VISIBLE);
+                binding.tLogo.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                binding.appName.startAnimation(animation2);
+                binding.tName.startAnimation(animation2);
+
             }
 
             @Override
@@ -104,20 +107,37 @@ public class SplashActivity extends BaseActivity {
 
             }
         });
-
         animation2.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                binding.appName.setVisibility(View.VISIBLE);
+                binding.tName.setVisibility(View.VISIBLE);
+
+            }
+
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                binding.buttonLinear.startAnimation(animation3);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        animation3.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+                binding.buttonLinear.setVisibility(View.VISIBLE);
+
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                if (getUserModel() == null) {
-                    navigateToLoginActivity();
-                } else {
-                    navigateToHomeActivity();
-                }
+
             }
 
             @Override
@@ -129,26 +149,27 @@ public class SplashActivity extends BaseActivity {
     }
 
     private void navigateToLoginActivity() {
-        Intent intent =new Intent(this,LoginActivity.class);
-        startActivity(intent);
-        finish();
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashhActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
 
     private void navigateToHomeActivity() {
-        Intent intent;
-        if (preferences.getAppSetting(this) == null ) {
-            intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-
-
-            intent = new Intent(this, HomeActivity.class);
-
+    binding.guestBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(SplashhActivity.this, HomeActivity.class);
             startActivity(intent);
             finish();
         }
+    });
     }
 
     @Override
@@ -156,4 +177,5 @@ public class SplashActivity extends BaseActivity {
         super.onDestroy();
         disposable.clear();
     }
+
 }
